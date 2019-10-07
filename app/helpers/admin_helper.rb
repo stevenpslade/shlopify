8b000a436cb326a4587adfd7aa2bb0471e6a476b
stevenpslade <stevenpslade@gmail.com>
2019-10-07 16:55:25 -0700

    adding subitem menu and putting logic in helper and partials for rendering

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,15 @@
+module AdminHelper
+  def get_subitems_by_controller(controller_name)
+    subitems = []
+    case controller_name
+    when 'products'
+      subitems << {:title => 'All Products', :link => admin_products_path}
+      subitems << {:title => 'Add Product', :link => new_admin_product_path}
+    when 'orders'
+      subitems << {:title => 'All Orders', :link => admin_orders_path}
+      subitems << {:title => 'Add Order', :link => new_admin_order_path}
+    end
+
+    return subitems
+  end
+end
