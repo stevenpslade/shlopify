40759e571f9159f5e19b64f641badac78a07fa97
stevenpslade <stevenpslade@gmail.com>
2019-11-03 15:01:55 -0800

    properly using webpacker and using turbolinks, js, etc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,10 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import '../css/application.css'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "../css/application.css";
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
