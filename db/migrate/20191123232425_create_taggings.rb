68bb38f5d37637eb74879fadddf9497f6992aced
stevenpslade <stevenpslade@gmail.com>
2019-11-23 15:27:19 -0800

    creating tag migration

---
+++ db/migrate/20191123232425_create_taggings.rb
@@ -0,0 +1,11 @@
+class CreateTaggings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :taggings do |t|
+      t.integer :taggings_count
+      t.belongs_to :product, foreign_key: true
+      t.belongs_to :store, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
