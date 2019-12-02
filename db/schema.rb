149a3126740a2ef18cc9268dbd2a3f7561cb6c98
stevenpslade <stevenpslade@gmail.com>
2019-12-01 22:20:34 -0800

    adding index to tags table on name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_002358) do
+ActiveRecord::Schema.define(version: 2019_12_02_061845) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -163,6 +163,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_002358) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
 end
