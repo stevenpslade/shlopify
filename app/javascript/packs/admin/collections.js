94ca6d242a92e0f4ebbc1831389aec21a155b8f7
stevenpslade <stevenpslade@gmail.com>
2019-12-01 20:14:42 -0800

    image upload for colleciton image

--- app/javascript/packs/admin/collections.js
+++ app/javascript/packs/admin/collections.js
@@ -1,9 +1,9 @@
 setupReader = file => {
-  var name = file.name;
   var reader = new FileReader();  
   reader.onload = function(e) {
     let img       = document.createElement('img');
     img.src       = e.target.result;
+    img.id        = 'collectionImage';
     img.className = 'block h-full w-full bg-white object-contain border rounded border-gray-300';
 
     let collectionImageContainer = document.getElementById('collectionImageContainer');
@@ -13,13 +13,18 @@ setupReader = file => {
   reader.readAsDataURL(file);
 };
 
-document.getElementById('collection_image').onchange = () => {
+document.getElementById('collection_image').onchange = e => {
   document.getElementById('addImageLabel').classList.add('hidden');
-  for (var i = 0; i < this.files.length; i++) {
-    setupReader(this.files[i]);
+  for (var i = 0; i < e.target.files.length; i++) {
+    setupReader(e.target.files[i]);
   }
+
+  document.getElementById('removeImage').classList.remove('hidden');
 };
 
-document.getElementById('removeImage').onclick = () => {
-  console.log('clicked');
+document.getElementById('removeImage').onclick = e => {
+  document.getElementById('collection_image').value = '';
+  document.getElementById('collectionImage').remove();
+  document.getElementById('addImageLabel').classList.remove('hidden');
+  e.target.classList.add('hidden');
 };
---
+++ app/javascript/packs/admin/products.js
@@ -0,0 +1,32 @@
+setupReader = file => {
+  var name = file.name;
+  var reader = new FileReader();  
+  reader.onload = function(e) {
+    let img   = document.createElement('img');
+    img.src   = e.target.result;
+    img.width = '160';
+
+    let div       = document.createElement('div');
+    div.className = 'p-2';
+    div.appendChild(img);
+
+    let imagePreviewContainer = document.getElementById('imagePreviewContainer');
+    imagePreviewContainer.appendChild(div);
+  }
+
+  reader.readAsDataURL(file);
+}
+
+document.getElementById('product_images').onchange = () => {
+  let imagePreviewContainer = document.getElementById('imagePreviewContainer');
+  imagePreviewContainer.innerHTML = '';
+  imagePreviewContainer.classList.remove('hidden');
+  for (var i = 0; i < this.files.length; i++) {
+    setupReader(this.files[i]);
+  }
+};
+
+
+document.body.addEventListener('ajax:success', function(event) {
+  event.target.parentNode.remove();
+});
