f61948f77ed482518030084dea11a1810a725b82
stevenpslade <stevenpslade@gmail.com>
2019-12-28 13:18:09 -0400

    adding stripe url button herlper

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SettingsHelper
+  def stripe_button_link
+    stripe_url = 'https://connect.stripe.com/express/oauth/authorize'
+    redirect_uri = stripe_connect_url
+    client_id = ENV['STRIPE_CLIENT_ID']
+  
+    "#{stripe_url}?redirect_uri=#{redirect_uri}&client_id=#{client_id}"
+  end
 end
