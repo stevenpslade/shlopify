de9732613bf2eebd94d6484928aa1975f559b8be
stevenpslade <stevenpslade@gmail.com>
2019-12-29 02:20:27 -0400

    Upgrade from Rails 5.2 to 6.0.2 and Ruby from 2.4 to 2.7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= require @rails/ujs
+//= require @rails/activestorage
+//= require @rails/turbolinks
 //= require_tree .
