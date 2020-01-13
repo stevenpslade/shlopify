9083cfd0d29ae71aa294029d6ce6c9fa16682c74
stevenpslade <stevenpslade@gmail.com>
2020-01-12 19:27:16 -0800

    Using Rails logger to log Stripe connect failures

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -4,21 +4,28 @@ class StripeController < ApplicationController
     # /stripe/connect is called from outside the admin site.
     # we thus need to actually grab the current store from the logged in admin user and
     # ensure that a stripe_user_id has not already been connected to the store
-    current_store = @current_user.stores.first
+    current_store = current_user.stores.first
     begin
       raise 'Error connecting Stripe account.' if current_store.stripe_user_id || !params[:code]
       Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+      byebug
       response = Stripe::OAuth.token({
         grant_type: 'authorization_code',
         code: params[:code],
       })
 
-      raise response.error_description if response.error
       current_store.stripe_user_id = response.stripe_user_id
       current_store.save!
 
       redirect_to admin_settings_url(subdomain: current_store.subdomain, notice: 'Stripe successfully connected!')
     rescue StandardError => e
+      # Log Stripe connect failures
+      if e.error.error && e.error.error_description
+        logger.tagged('Stripe', e.error.error) do
+          logger.error "#{e.error.error_description} for Store ID: #{current_store.id}"
+        end
+      end
+
       redirect_to admin_settings_url(subdomain: current_store.subdomain, notice: e.message)
     end
   end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Logger setup
+Rails.logger = Logger.new(STDOUT)
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
