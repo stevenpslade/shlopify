2270f8b53fce73579f167cffa7d76c81579f23f4
stevenpslade <stevenpslade@gmail.com>
2020-03-15 22:15:53 -0700

    prepending theme path to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_store
+  before_action :set_current_store, :get_theme
+
+  # prepend view path with theme folder
+  prepend_view_path "app/views/#{@theme_path}"
 
   protect_from_forgery with: :exception
   include Admin::SessionsHelper
@@ -17,5 +20,10 @@ class ApplicationController < ActionController::Base
           redirect_to root_url(subdomain: logged_in? ? @current_user.stores.first.subdomain : false)
         end
       end
+    end
+
+    def get_theme
+      # TODO: store 'thetheme' in theme table and relate to store
+      @theme_path = 'themes/thetheme'
     end
 end
